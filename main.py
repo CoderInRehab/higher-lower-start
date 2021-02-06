e6bb04a5209497b6ce78684a1c81cde46a5530bc
CoderInRehab <happy109rocks@gmail.com>
2021-02-06 13:55:30 +0000

    Some adjustments in code that makes easier to understand, also use of functions increased

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import random
 from game_data import data
 from replit import clear
-import art 
+from art import logo,vs  
 #compare with original answer using functions
 
 def compare(user_guess,answer):
@@ -11,53 +11,55 @@ def compare(user_guess,answer):
   else:
     return 0
 
+def format_data(person):
+  person_name = person["name"]
+  person_descr = person["description"]
+  person_country = person["country"]
+  return f"{person_name}, a {person_descr}from {person_country}"
+
+def check_answer(user_guess, value_a, value_b):
+
+  #print(value_a, value_b)
+  if value_a > value_b:
+    return user_guess == "a"
+  else:
+    return user_guess == "b"
 def game():
-  
+  print(logo)
   person_a = random.choice(data)
 
   is_game_over = False
   score = 0
 
   while not is_game_over:
-    print(art.logo)
+   
     person_b = random.choice(data)
-    if person_a == person_b:
+    while person_a == person_b:
       person_b = random.choice(data)
       
-    print(f"Current score: {score}")
     # print details of person_a and person_b
 
-    print(f"Compare A: {person_a['name']}, {person_a['description']} from {person_a['country']} ")
-
-    print(art.vs)
-
-    print(f"Against B: {person_b['name']}, {person_b['description']} from {person_b['country']} ")
+    print(f"Compare A: {format_data(person_a)} ")
+    print(vs)
+    print(f"Against B: {format_data(person_b)} ")
 
     #calculate original answer
 
     value_a = person_a['follower_count']
     value_b = person_b['follower_count']
 
-    #print(value_a, value_b)
-    if value_a > value_b:
-      answer_value = value_a
-      answer = "A"
-    else :
-      answer_value = value_b
-      answer = "B"
-
     #ask the user for an answer a and b?
-    user_guess = input("Who has more followers? Type 'A' or 'B' ")
-    print(user_guess)
-    var = compare(answer,user_guess)
-    if var == 1:
+    user_guess = input("Who has more followers? Type 'A' or 'B' ").lower()
+    is_correct = check_answer(user_guess, value_a, value_b)
+    clear() 
+    print(logo)    
+    if is_correct:
       score += 1
-      clear() 
-      
+      print(f"You're right! Current score: {score}.")
       person_a = person_b
       
     else:
-      print(f"Sorry you lose. Final score: {score}")
+      print(f"Sorry that's wrong. Final score: {score}")
       is_game_over = True
 
 game()
